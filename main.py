d350f6067cb4faa5987931c3438b771367d983e0
Kamalkoushik <77505259+Kamalkoushik@users.noreply.github.com>
2022-01-20 13:54:37 +0530

    1st commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-d = {"30506": "koushik", "30532": "harshith", "30317": "leelu"}
+d = {"30506": "koushik", "30532": "harshith", "30420": "leelu"}
 
 df = pd.DataFrame(d)
 print(df)
